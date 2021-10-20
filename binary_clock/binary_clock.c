e13a8901d0c2d71984d4ea34941d5624600518af
Ted Percival <ted@tedp.id.au>
2021-10-20 00:12:01 -0700

    Run on main thread to avoid drift

--- binary_clock/binary_clock.c
+++ binary_clock/binary_clock.c
@@ -2,27 +2,24 @@
 #include "hardware/gpio.h"
 #include "pico/time.h"
 
-static uint32_t gCounter = 0;
-
 static const uint32_t PIN_MASK = 0x1ffffu; // 17 bits; pins GP0 -> GP16
 
-static bool update_clock(repeating_timer_t *rt) {
-    ++gCounter;
+uint32_t add_tick(uint32_t secs) {
+    ++secs;
+    // wrap daily
+    secs %= 86400;
+    return secs;
+}
 
-    // Reset daily
-    gCounter %= 86400;
-
-    uint32_t pinState = gCounter;
+static void update_pins(uint32_t value) {
+    uint32_t pinState = value;
 
 #if defined(PICO_DEFAULT_LED_PIN)
     // Set default LED to low bit too
-    pinState |= (gCounter & 0x01) << PICO_DEFAULT_LED_PIN;
+    pinState |= (value & 0x01) << PICO_DEFAULT_LED_PIN;
 #endif
 
     gpio_put_all(pinState);
-
-    // return true to keep repeating
-    return true;
 }
 
 static void setup() {
@@ -36,14 +33,16 @@ static void setup() {
     gpio_set_dir_out_masked(pinMask);
 }
 
-int main() {
+void main() {
     setup();
 
-    static repeating_timer_t timer_1hz;
-    // negative interval schedules the start times to align; positive interval
-    // waits the duration after each callback completes, which would drift.
-    add_repeating_timer_ms(-1000, &update_clock, NULL, &timer_1hz);
+    absolute_time_t nextWakeup = get_absolute_time();
+    uint32_t secs = 0;
 
-    for (;;)
-        sleep_ms(1000); // FIXME: This probably drifts
+    for (;;) {
+        nextWakeup = delayed_by_ms(nextWakeup, 1000);
+        sleep_until(nextWakeup);
+        secs = add_tick(secs);
+        update_pins(secs);
+    }
 }
