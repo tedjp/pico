3e01afb7bebb2b65c4c004b3500d56ea73536e73
Ted Percival <ted@tedp.id.au>
2021-10-20 01:15:58 -0700

    Don't use builtin LED

--- binary_clock/binary_clock.c
+++ binary_clock/binary_clock.c
@@ -4,46 +4,33 @@
 
 static const uint32_t PIN_MASK = 0x1ffffu; // 17 bits; pins GP0 -> GP16
 
-uint32_t add_tick(uint32_t semiSecs) {
-    ++semiSecs;
+uint32_t add_tick(uint32_t secs) {
+    ++secs;
     // wrap daily
-    semiSecs %= 86400;
-    return semiSecs;
+    secs %= 86400;
+    return secs;
 }
 
-static void set_gpio_pins(uint32_t semiSecs) {
-    uint32_t pinState = semiSecs / 2;
-
-#if defined(PICO_DEFAULT_LED_PIN)
-    // Set default LED to low bit too
-    pinState |= (semiSecs & 0x01) << PICO_DEFAULT_LED_PIN;
-#endif
-
-    gpio_put_all(pinState);
+static void set_gpio_pins(uint32_t secs) {
+    gpio_put_all(secs);
 }
 
 static void setup() {
-    uint32_t pinMask = PIN_MASK;
-
-#if defined(PICO_DEFAULT_LED_PIN)
-    pinMask |= 1u << PICO_DEFAULT_LED_PIN;
-#endif
-
-    gpio_init_mask(pinMask);
-    gpio_set_dir_out_masked(pinMask);
+    gpio_init_mask(PIN_MASK);
+    gpio_set_dir_out_masked(PIN_MASK);
 }
 
 int main() {
     setup();
 
     absolute_time_t nextWakeup = get_absolute_time();
-    uint32_t semiSecs = 0;
+    uint32_t secs = 0;
 
     for (;;) {
-        nextWakeup = delayed_by_ms(nextWakeup, 500);
+        nextWakeup = delayed_by_ms(nextWakeup, 1000);
         sleep_until(nextWakeup);
-        semiSecs = add_tick(semiSecs);
-        set_gpio_pins(semiSecs);
+        secs = add_tick(secs);
+        set_gpio_pins(secs);
     }
 
     return 0;
